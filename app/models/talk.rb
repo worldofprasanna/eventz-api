045d2afe024dc07454dd797a4a3f41bfadc9f2b0
Prasanna V <prasannacse2005@gmail.com>
2020-02-19 18:36:25 +0530

    Allow speaker to be nil

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -2,6 +2,6 @@ class Talk < ApplicationRecord
   belongs_to :speaker, optional: true
   belongs_to :conference, optional: true
 
-  delegate :name, to: :speaker, prefix: :speaker
-  delegate :profile_pic, to: :speaker, prefix: :speaker
+  delegate :name, to: :speaker, prefix: :speaker, allow_nil: true
+  delegate :profile_pic, to: :speaker, prefix: :speaker, allow_nil: true
 end
