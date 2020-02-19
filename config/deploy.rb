c7bc683419332a73a5189a43bd62f24f3a45b551
Prasanna V <prasannacse2005@gmail.com>
2020-02-19 18:43:07 +0530

    link the .env file to production

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ set :puma_init_active_record, true
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml"
 append :linked_files, "config/master.key"
+append :linked_files, ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public"
